e574843472e132040a4098d24a63e8183c839519
Gabriella Pacheco <gabriiiella.pacheco@gmail.com>
2024-04-13 11:16:59 +0000

    Cleaned tables (campaign name and casted ads_cost as float)

--- models/staging/raw/stg_raw__criteo.sql
+++ models/staging/raw/stg_raw__criteo.sql
@@ -12,8 +12,8 @@ renamed as (
         date_date,
         paid_source,
         campaign_key,
-        campgn_name,
-        ads_cost,
+        campgn_name as campaign_name,
+        CAST(ads_cost AS float64) AS ads_cost,
         impression,
         click
 
